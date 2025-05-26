d6d16c5240d542f38541572a3056ecb5bfea32ed
bain-liao-1 <liao.bain@stu.ocsb.ca>
2025-05-26 23:23:29 +0000

    created program for arithmetic sequences

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bain Liao
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+* This function calculates the arithmetic sequence.
+*/
+function calculateArithmeticSequence() {
+  // User input
+  const startingValue = parseInt(document.getElementById("starting-value").value)
+  const commonDifference = parseInt(document.getElementById("common-difference").value)
+  const numberOfTerms = parseInt(document.getElementById("number-of-terms").value)
+  
+  let arithmeticSequence = ""
+  
+  // Process
+  for (let remainingTerms = numberOfTerms; remainingTerms > 0; remainingTerms--) {
+    let currentTerm = startingValue + ((numberOfTerms - remainingTerms) * commonDifference)
+    arithmeticSequence += currentTerm + ", "
+  }
+  
+  // Output
+  document.getElementById("result").innerText = "The arithmetic sequence is: " + arithmeticSequence
+}
